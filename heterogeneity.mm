66707988469122f36204fbb8adc8bde56197bca9
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-06-09 18:00:10 +0100

    Scotland

--- heterogeneity.mm
+++ heterogeneity.mm
@@ -1,17 +1,2 @@
-MC If mean parameters are held constant, what is the effect of increasing variation in mixing rates on R0?
-* R0 increases
-R0 decreases
-R0 first increases, then decreases
-It depends
 
-MC If R0 is held constant, what is the effect of increasing variation in mixing rates on equilibrium prevalence?
-Equilibrium prevalence increases
-* Equilibrium prevalence decreases
-Equilibrium prevalence first increases, then decreases
-It depends
-
-MC If mean parameters are held constant, what is the effect of increasing variation in mixing rates on equilibrium prevalence?
-Equilibrium prevalence increases
-* Equilibrium prevalence decreases
-Equilibrium prevalence first increases, then decreases
-It depends
+https://docs.google.com/document/d/1efu4SbNDGIui5uo_WCcpmY_rHvUs6fEx/edit
