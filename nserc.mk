fbdf861e10f05000336d8d85f5296dc57132983a
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-10-27 17:15:16 -0400

    attic

--- nserc.mk
+++ nserc.mk
@@ -4,5 +4,5 @@ mixingPitch.png: SIR_simulations/mpHIVplots.pitch.pdf caseVariation/figs/pitch.R
 	convert -density 600 $< $(word 2, $^) $(word 3, $^) +append $@
 
 mixingPitch.factors.png: my_images/modelFactors.png my_images/modelProduct.png nserc.mk
-	convert -density 600 $< $(word 2, $^) -append $@
+	convert -extent 1359x1229 -gravity center -density 600 $< $(word 2, $^) -append $@
 
