3dd87ac63e45ee73936ae17e2bf41a07bb709c64
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-06-16 09:41:05 -0400

    Teaching computer…

--- family.mm
+++ family.mm
@@ -1,2 +1,39 @@
 
-https://docs.google.com/document/d/1efu4SbNDGIui5uo_WCcpmY_rHvUs6fEx/edit
+2025
+* https://docs.google.com/document/d/1efu4SbNDGIui5uo_WCcpmY_rHvUs6fEx/edit
+
+Q. Which of the following factors can _decrease_ the effective reproductive number in the model world of the simple SIR model (check as many as apply)?
+
+Behavioural changes
+
+Depletion of susceptibles
+
+Differences among people
+
+Policy interventions
+
+ANS Depletion only
+
+Q. Which of the following factors can _decrease_ the effective reproductive number in the real world (check as many as apply)?
+
+Behavioural changes
+
+Depletion of susceptibles
+
+Differences among people
+
+Policy interventions
+
+ANS All of the above
+
+Q. To run our live-coding implementation of the simple SIR, we needed to specify
+
+The number of people in each state at the beginning
+
+The period of seasonal forcing
+
+The size of the time step
+
+The values of the parameters underlying transmission and recovery
+
+ANS ACD
